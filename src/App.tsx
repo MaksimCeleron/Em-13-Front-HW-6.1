160d8a5d5b245ea064529a05ce49feab44628b51
MaksimCeleron <143294016+MaksimCeleron@users.noreply.github.com>
2025-08-27 08:22:57 +0000

    Update page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <div className="wrapper">
         <header className="container header">
           <div className="nav">
             <ul>
-              <li><a href="/">MAIN</a></li>
-              <li><a href="/gallery">GALLERY</a></li>
-              <li><a href="/projects">PROJECTS</a></li>
-              <li><a href="/certifications">CERTIFICATIONS</a></li>
-              <li><a href="/contacts">CONTACTS</a></li>
+              <li>
+                <a href="/">MAIN</a>
+              </li>
+              <li>
+                <a href="/gallery">GALLERY</a>
+              </li>
+              <li>
+                <a href="/projects">PROJECTS</a>
+              </li>
+              <li>
+                <a href="/certifications">CERTIFICATIONS</a>
+              </li>
+              <li>
+                <a href="/contacts">CONTACTS</a>
+              </li>
             </ul>
           </div>
         </header>
         <main className="container content">
-          <h1>PROJECT<br><b>Lorum</b></h1>
+          <h1>
+            <div className="light">PROJECT</div>
+            <div className="bold">Lorum</div>
+          </h1>
         </main>
-        <footer className="container footer">
-          MaksimCeleron 2025
-        </footer>
+        <footer className="container footer">MaksimCeleron 2025</footer>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
